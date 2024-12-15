b328565492ed0f458eb6785f1953d4c141db5201
joe-black-jb <joeblack3601@gmail.com>
2024-12-15 14:38:24 +0900

    fix input

--- front/src/components/Input.tsx
+++ front/src/components/Input.tsx
@@ -1,14 +1,16 @@
 interface Props {
   type: string;
   value: string;
+  placeholder?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 const Input = (props: Props) => {
-  const { type, value, onChange } = props;
+  const { type, value, placeholder, onChange } = props;
   return (
     <input
       type={type}
       value={value}
+      placeholder={placeholder}
       onChange={onChange}
       className="px-2 py-1 border border-gray-500 rounded-lg"
     />
